d1ed83d520f37974fb02014f0628b57db83f4a00
naishe <naisherowland57@gmail.com>
2025-10-27 12:35:51 +0200

    created an activationView , this is the basic view but its going to lead you to a more detaield view

---
+++ QuickCap 2 /Core/LocationSearch/Views/LocationSearchActivationView.swift
@@ -0,0 +1,36 @@
+//
+//  LocationSearchActivationView.swift
+//  QuickCab2
+//
+//  Created by Naishe Rowland  on 10/27/25.
+//
+
+import SwiftUI
+
+struct LocationSearchActivationView: View {
+    var body: some View {
+        HStack{
+            
+            Rectangle()
+                .fill(Color.black)
+                .frame(width: 8, height: 8)
+                .padding(.horizontal)
+            
+            Text("Where to ?")
+                .foregroundStyle(Color(.darkGray))
+            
+            Spacer()
+                
+        }
+        .frame(width: UIScreen.main.bounds.width - 64, height: 50)
+        .background(
+            Rectangle()
+                .fill(Color(.white))
+                .shadow(color: .black, radius: 6)
+        )
+    }
+}
+
+#Preview {
+    LocationSearchActivationView()
+}
